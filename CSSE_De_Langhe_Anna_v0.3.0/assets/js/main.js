3ac3abf03a19cda809ca713f704e9c980f0df555
gdm-1718-annadela <annadela@student.arteveldehs.be>
2019-04-02 15:14:07 +0200

    V0.3.0
    CleanCode

---
+++ CSSE_De_Langhe_Anna_v0.3.0/assets/js/main.js
@@ -0,0 +1,11 @@
+/***************************************************
+* @author       Parent Olivier, Anna De Langhe
+* @created      27.02.2019
+* @modified     02.04.2019
+* @copyright    Copyright © 2017-2018 Arteveldehs
+* @function     Canvas Drawing Clock
+***************************************************/
+
+import Application from './Application.js'
+
+window.addEventListener('DOMContentLoaded', _ => new Application(), false)
